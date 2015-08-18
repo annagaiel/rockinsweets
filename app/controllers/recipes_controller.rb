cf1e43fed30a3284683762bf2ca7f9616b9a9d8f
Anna Natorilla <annagaiel@gmail.com>
2015-08-18 10:24:37 +0000

    add recipe index and show, add their action on controller, add validation on recipe model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,21 +1,29 @@
 class RecipesController < ApplicationController
-	def new
-		@recipe = Recipe.new
-	end
 
-	def create
-	@recipe = Recipe.new(recipe_params)
+		def index
+			@recipe = Recipe.all
+		end
 
-	@recipe.save
-	redirect_to @recipe
-	end
+		def new
+			@recipe = Recipe.new
+		end
 
-	private
-	def recipe_params
-		params.require(:recipe).permit(:title, :text)
-	end
+		def create
+			@recipe = Recipe.new(recipe_params)
 
-	def show
-	 @recipe = Recipe.find(params[:id])
- end
+			if @recipe.save
+					 redirect_to @recipe
+			else
+				render 'new'
+			end
+		end
+
+		def show
+			@recipe = Recipe.find(params[:id])
+		end
+
+		 private
+		 def recipe_params
+			 params.require(:recipe).permit(:title, :text)
+		 end
 end
