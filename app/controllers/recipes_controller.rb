57f96d2596df1588fef2e8184be7e56a117f73c4
Anna Natorilla <annagaiel@gmail.com>
2015-08-18 04:00:05 +0000

    Add simple form and add the create recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,21 @@
 class RecipesController < ApplicationController
 	def new
+		@recipe = Recipe.new
 	end
+
+	def create
+	@recipe = Recipe.new(recipe_params)
+
+	@recipe.save
+	redirect_to @recipe
+	end
+
+	private
+	def recipe_params
+		params.require(:recipe).permit(:title, :text)
+	end
+
+	def show
+	 @recipe = Recipe.find(params[:id])
+ end
 end
